3857fc5035ca7ff5a9be5bc2cbaabeab3e8dc79b
ChoiWonSeo <w2111@e-mirim.hs.kr>
2023-06-20 01:26:02 +0900

    rooms 수정

--- src/pages/rooms.jsx
+++ src/pages/rooms.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Community from './community';
 import Study from './study';
 
