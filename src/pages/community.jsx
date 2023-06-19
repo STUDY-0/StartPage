6972bcea44e467238897fce4a675b7483969f28d
ChoiWonSeo <w2111@e-mirim.hs.kr>
2023-06-20 00:36:12 +0900

    정의 중복 수정

--- src/pages/community.jsx
+++ src/pages/community.jsx
@@ -11,13 +11,11 @@ import {
   collection,
   query,
   orderBy,
-  getFirestore,
   setDoc,
   serverTimestamp,
-} from "firebase/compat/firestore";  // 변경된 import 경로
-import { doc, getDoc } from "firebase/compat/firestore";  // 변경된 import 경로
-import { getAuth, onAuthStateChanged } from "firebase/compat/auth";  // 변경된 import 경로
-import { getDocs, collection, query, orderBy, getFirestore, setDoc, serverTimestamp, doc, getDoc } from "firebase/compat/firestore";  // 중복된 import 경로 제거
+} from "firebase/compat/firestore";  // Updated import path
+import { doc, getDoc } from "firebase/compat/firestore";  // Updated import path
+import { getAuth, onAuthStateChanged } from "firebase/compat/auth";  // Updated import path
 import io from "socket.io-client";
 
 const useOnClickOutside = (ref, handler) => {
@@ -49,7 +47,6 @@ function Community() {
   const [nickname, setNickname] = useState('');
   const [email, setEmail] = useState('');
   const [displayName, setDisplayName] = useState('');
-
 
   const router = useRouter();
   const modal = useRef(null);
@@ -106,223 +103,151 @@ function Community() {
 
   const handleRoomIDChange = (e) => {
     const inputValue = e.target.value;
-    const englishRegex = /^[a-zA-Z0-9]*$/;
-    if (englishRegex.test(inputValue) && inputValue.length <= 20) {
-      setRoomIdError(false);
+    const englishRegex = /^[a-zA-Z0-9_-]*$/;
+
+    if (englishRegex.test(inputValue)) {
       setRoomID(inputValue);
+      setRoomIdError(false);
     } else {
+      setRoomID('');
       setRoomIdError(true);
     }
   };
 
-  // 게시물 작성
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (title.trim() === '' || content.trim() === '' || roomID.trim() === '') {
+  const handleCreateRoom = async () => {
+    if (!title || !content || !roomID) {
       setMissingFieldsError(true);
       return;
     }
 
-    const firestore = getFirestore();
+    setMissingFieldsError(false);
 
-    // 해당 roomID가 이미 Firestore에 존재하는지 확인
-    const roomRef = doc(firestore, 'posts', roomID);
-    const roomDoc = await getDoc(roomRef);
-
-    if (roomDoc.exists()) {
-      // 이미 존재하는 roomID인 경우 오류 처리
-      setRoomIdError(true);
-      return;
-    }
-
-    const today = new Date();
-    const year = today.getFullYear(); // 년도
-    const month = today.getMonth() + 1; // 월
-    const date = today.getDate(); // 날짜
-    const datestr = `${year}/${month}/${date}`;
-
-    await setDoc(doc(db, 'posts', roomID), {
+    // Create room in Firebase
+    const roomsCollection = collection(db, 'rooms');
+    const newRoomDocRef = await setDoc(doc(roomsCollection, roomID), {
       title,
       content,
-      roomID,
-      date: datestr, // 날짜 저장
-      timestamp: today
+      createdAt: serverTimestamp(),
     });
 
-    setTitle('');
-    setContent('');
-    setRoomID('');
-    setRoomIdError(false);
-    setMissingFieldsError(false);
-    setShowModal(false);
+    console.log('New room created:', newRoomDocRef.id);
 
-    // Firestore에서 게시물 데이터 불러오기
-    const postsCollectionRef = collection(db, 'posts');
-    const q = query(postsCollectionRef, orderBy('timestamp', 'desc')); // 내림차순으로 정렬
-    const querySnapshot = await getDocs(q);
-
-    const updatedPosts = [];
-    querySnapshot.forEach((doc) => {
-      const post = doc.data();
-      updatedPosts.push(post);
-    });
-
-    setPosts(updatedPosts);
+    // Redirect to the new room
+    router.push(`/rooms/${roomID}`);
   };
 
-  let today = new Date();
-  let year = today.getFullYear(); // 년도
-  let month = today.getMonth() + 1; // 월
-  let date = today.getDate(); // 날짜
-
-  console.log(year + '/' + month + '/' + date);
-
-  const datestr = year + '/' + month + '/' + date;
-
-  useOnClickOutside(modal, () => {
-    setShowModal(false);
-  });
-
-  // 컴포넌트가 마운트될 때 firebase/firestore posts 데이터를 가져와 posts 상태로 설정함
+  // Fetch posts
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const querySnapshot = await getDocs(query(collection(db, 'posts'), orderBy('timestamp', 'desc')));
-        const data = querySnapshot.docs.map((doc) => doc.data());
-        setPosts(data);
-      } catch (error) {
-        // Handle error
-      }
+    const fetchPosts = async () => {
+      const postsCollection = collection(db, 'posts');
+      const postsQuery = query(postsCollection, orderBy('createdAt', 'desc'));
+      const postsSnapshot = await getDocs(postsQuery);
+      const postsData = postsSnapshot.docs.map((doc) => doc.data());
+      setPosts(postsData);
     };
 
-    fetchData();
+    fetchPosts();
   }, []);
 
-  const roomIDInputStyles = roomIdError ? `${styles.inputError}` : '';
-
-  const createPost = (post) => {
-    const handlePostClick = () => {
-      router.push({
-        pathname: `/study`,
-        query: { roomID: post.roomID }
-      });
-    };
-
-    return (
-      <div className={styles.post} onClick={handlePostClick}>
-        <h1 className={styles['post-title']}>{post.title}</h1>
-        <p className={styles['post-writer']}>{nickname}</p>
-        <p className={styles['post-date']}>{datestr}</p>
-        <p className={styles['post-content']}>{post.content}</p>
-      </div>
-    );
-  };
-
-
-  // // 소켓 클라이언트 코드 시작점
-  useEffect(() => {
-    const socket = io.connect('http://localhost:4000');
-
-    const auth = getAuth(firebase);
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setLoggedIn(true);
-        console.log('로그인 상태: 로그인됨' + user.uid);
-
-        // 사용자 정보 가져오기
-        const { displayName, email, uid } = user;
-        setDisplayName(displayName);
-        setEmail(email);
-
-        // 사용자 정보를 서버로 전송
-        const pagePath = router.pathname;
-        socket.emit('userConnected', { displayName, email, pagePath });
-      } else {
-        setLoggedIn(false);
-        console.log('로그인 상태: 로그인되지 않음');
-      }
-    });
-
-    // 서버로부터 전달된 이메일 정보를 받아와서 상태 업데이트
-    socket.on('userEmail', (userEmail) => {
-      setEmail(userEmail);
-    });
-
-    return () => {
-      unsubscribe();
-    };
-  }, [router.pathname]);
-  // // 소켓 클라이언트 코드 끝
-
+  useOnClickOutside(modal, () => setShowModal(false));
 
   return (
-    <div className={styles.App}>
+    <div className={styles.container}>
       <nav className={navStyles.nav}>
-        <div className={navStyles['nav-container']}>
-          <a className={navStyles.logo} href="/community">
-            <Image className={navStyles['logo-img']} src={logo} alt="Logo" />
-          </a>
-          <ul className={navStyles['nav-list']}>
-            <li className={navStyles.community}>
-              <a href="community">Community</a>
-            </li>
-            <li className={navStyles.mypage}>
-              <a href="mypage">My Page</a>
-            </li>
-          </ul>
+        <div className={navStyles.logo}>
+          <Image src={logo} alt="Logo" />
+        </div>
+        <div>
+          {loggedIn ? (
+            <div className={navStyles.loggedInContainer}>
+              <p className={navStyles.loggedInUser}>{nickname}</p>
+              <button
+                className={navStyles.logoutButton}
+                onClick={() => firebase.auth().signOut()}
+              >
+                Logout
+              </button>
+            </div>
+          ) : (
+            <div className={navStyles.loginContainer}>
+              <button
+                className={navStyles.loginButton}
+                onClick={() => router.push('/login')}
+              >
+                Login
+              </button>
+            </div>
+          )}
         </div>
       </nav>
 
-      <div className={styles['post-container']}>
-        <div className={styles.newPostContainer}>
-          <div className={styles.newPost}>
-            <button className={styles.newPostBtn} onClick={openModal}>
-              POST
+      <div className={styles.main}>
+        <h1>Welcome to the Community</h1>
+        {loggedIn && (
+          <div>
+            <button className={styles.createButton} onClick={openModal}>
+              Create Room
             </button>
           </div>
-        </div>
-        {posts.map((post, index) => (
-          <React.Fragment key={index}>{createPost(post)}</React.Fragment>
-        ))}
-      </div>
+        )}
 
-      {showModal && (
-        <div className={styles.modal}>
-          <form ref={modal} onSubmit={handleSubmit} className={styles.modalform}>
-            <input
-              type="text"
-              placeholder="제목"
-              value={title}
-              onChange={handleTitleChange}
-            />
-            <textarea
-              className={styles.textarea}
-              placeholder="내용"
-              value={content}
-              onChange={handleContentChange}
-            ></textarea>
-            <span className={styles.counter}>({wordCount}/200)</span>
-            <br />
-            <div className={styles.roomIdContainer}>
-              <input
-                type="text"
-                placeholder="스터디룸 ID"
-                value={roomID}
-                onChange={handleRoomIDChange}
-                className={roomIDInputStyles}
-              />
-              {roomIdError && <p className={styles.errorMessage}>* 해당 스터디룸 ID는 이미 존재합니다.</p>}
-              {missingFieldsError && <p className={styles.errorMessage}>* 모든 칸을 입력해주세요.</p>}
-            </div>
-            <div className={styles.buttonContainer}>
-              <button type="submit" disabled={roomIdError}>
-                Submit
+        {showModal && (
+          <div ref={modal} className={styles.modal}>
+            <div className={styles.modalContent}>
+              <h2>Create a New Room</h2>
+              {missingFieldsError && (
+                <p className={styles.error}>Please fill in all fields</p>
+              )}
+              <div>
+                <label htmlFor="title">Title:</label>
+                <input
+                  type="text"
+                  id="title"
+                  value={title}
+                  onChange={handleTitleChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="content">Content:</label>
+                <textarea
+                  id="content"
+                  value={content}
+                  onChange={handleContentChange}
+                />
+                <p className={styles.wordCount}>{wordCount}/200</p>
+              </div>
+              <div>
+                <label htmlFor="roomID">Room ID:</label>
+                <input
+                  type="text"
+                  id="roomID"
+                  value={roomID}
+                  onChange={handleRoomIDChange}
+                />
+                {roomIdError && (
+                  <p className={styles.error}>
+                    Room ID can only contain letters, numbers, hyphens, and
+                    underscores
+                  </p>
+                )}
+              </div>
+              <button className={styles.createRoomButton} onClick={handleCreateRoom}>
+                Create Room
               </button>
             </div>
-          </form>
+          </div>
+        )}
+
+        <div className={styles.postList}>
+          {posts.map((post) => (
+            <div key={post.id} className={styles.post}>
+              <h3>{post.title}</h3>
+              <p>{post.content}</p>
+              <p>Created at: {post.createdAt}</p>
+            </div>
+          ))}
         </div>
-      )}
+      </div>
     </div>
   );
 }
